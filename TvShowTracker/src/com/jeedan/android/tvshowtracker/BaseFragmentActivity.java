2a8760115aff72e26bf475e8d53c4397eeacc8d1
Jeedan <Jeedan.Cuneyt@gmail.com>
2014-12-01 14:33:02 +0200

    features: loads from json searches tvrage updates tv show info when being added saves to json onPause

--- TvShowTracker/src/com/jeedan/android/tvshowtracker/BaseFragmentActivity.java
+++ TvShowTracker/src/com/jeedan/android/tvshowtracker/BaseFragmentActivity.java
@@ -1,5 +1,6 @@
 package com.jeedan.android.tvshowtracker;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,6 +10,7 @@ public abstract class BaseFragmentActivity extends FragmentActivity {
 	
 	protected abstract Fragment createFragment();
 	
+	@TargetApi(11)
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
